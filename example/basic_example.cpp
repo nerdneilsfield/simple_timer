1c9cb81e789f2afec9d0b5a4d3e38f2d03fc018a
dengqi <dengqi935@gmail.com>
2022-04-26 15:41:22 +0800

    example: short sleep time

--- example/basic_example.cpp
+++ example/basic_example.cpp
@@ -13,16 +13,16 @@ int main(int argc, char **argv) {
 
   simple_timer.start();
 
-  std::this_thread::sleep_for(std::chrono::seconds(1));
+  std::this_thread::sleep_for(std::chrono::milliseconds(300));
 
   simple_timer.stop();
 
   simple_timer.print_stats();
 
-  std::this_thread::sleep_for(std::chrono::seconds(1));
+  std::this_thread::sleep_for(std::chrono::milliseconds(300));
 
   simple_timer.start();
-  std::this_thread::sleep_for(std::chrono::seconds(1));
+  std::this_thread::sleep_for(std::chrono::milliseconds(300));
   simple_timer.stop();
   simple_timer.print_stats();
 
@@ -31,16 +31,16 @@ int main(int argc, char **argv) {
   SimpleTimerArrayInterface timer_array{name, 2};
 
   timer_array.start(0);
-  std::this_thread::sleep_for(std::chrono::seconds(1));
+  std::this_thread::sleep_for(std::chrono::milliseconds(300));
   timer_array.stop(0);
   timer_array.print_stats(0);
 
   timer_array.start(1);
-  std::this_thread::sleep_for(std::chrono::seconds(1));
+  std::this_thread::sleep_for(std::chrono::milliseconds(300));
   timer_array.stop(1);
   std::this_thread::sleep_for(std::chrono::milliseconds(100));
   timer_array.start(1);
-  std::this_thread::sleep_for(std::chrono::seconds(1));
+  std::this_thread::sleep_for(std::chrono::milliseconds(300));
   timer_array.stop(1);
   timer_array.print_all_stats();
 
@@ -59,7 +59,7 @@ int main(int argc, char **argv) {
 
   startTimer(&timer_instance, 1);
   startTimer(&timer_instance, 0);
-  std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+  std::this_thread::sleep_for(std::chrono::milliseconds(300));
   stopTimer(&timer_instance, 0);
   stopTimer(&timer_instance, 1);
   displayTimer(&timer_instance, 0);
@@ -69,11 +69,11 @@ int main(int argc, char **argv) {
   startTimer(&timer_instance, 1);
   startTimer(&timer_instance, 2);
 
-  std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+  std::this_thread::sleep_for(std::chrono::milliseconds(500));
 
   stopTimer(&timer_instance, 1);
 
-  std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+  std::this_thread::sleep_for(std::chrono::milliseconds(110));
   stopTimer(&timer_instance, 2);
 
   displayAllTimer(&timer_instance);
@@ -82,9 +82,9 @@ int main(int argc, char **argv) {
   double time2_elapsed = getTimerElapsed(&timer_instance, 1);
   double time3_elapsed = getTimerElapsed(&timer_instance, 2);
 
-  double time1_elapsed_ms = getTimerElapsed(&timer_instance, 0);
-  double time2_elapsed_ms = getTimerElapsed(&timer_instance, 1);
-  double time3_elapsed_ms = getTimerElapsed(&timer_instance, 2);
+  double time1_elapsed_ms = getTimerElapsedMs(&timer_instance, 0);
+  double time2_elapsed_ms = getTimerElapsedMs(&timer_instance, 1);
+  double time3_elapsed_ms = getTimerElapsedMs(&timer_instance, 2);
 
   deleteTimer(&timer_instance);
 }
